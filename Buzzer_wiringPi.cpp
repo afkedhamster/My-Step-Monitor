3148e9a9127894aaf6b6e3a4d095eba88dae4722
Administrator <Administrator@>
2024-03-20 15:30:07 +0000

    添加项目文件。

---
+++ Buzzer_wiringPi.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <wiringPi.h> 
+
+// 引脚
+#define BUZZER_PIN 0 // 板子
+
+// 初始化
+void wiringPi_st()
+{
+    if (wiringPiSetup() == -1) {
+        std::cerr << "Failed to initialize WiringPi library" << std::endl;
+        exit(1);
+    }
+}
+
+void setup()
+{
+    pinMode(BUZZER_PIN, OUTPUT); // 输出
+}
+
+// 发声
+void loop(int frequency, int duration)
+{
+    for (int i = 0; i < duration / 2; i++) {
+        digitalWrite(BUZZER_PIN, LOW); // 发声音
+        delay(1000 / frequency); // 延时1ms 
+        digitalWrite(BUZZER_PIN, HIGH); // 不发声音
+        delay(1000 / frequency); // 延时1ms 
+    }
+}
+
+int main()
+{
+    wiringPi_st();
+    setup();
+    loop(1000, 3000); // 1000Hz，1秒
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include <wiringPi.h>
+#include "Self_defined_classes.h"
+#include <iostream>
+
+Screen::Screen() {
+	// Initialization
+	std::cout << "Screen initialized" << std::endl;
+}
+
+void Screen::displayText(const std::string& text) {
+	// Display the text
+	std::cout << "Displaying text: " << text << std::endl;
+}
+
+void Screen::bmp_read(int address) {
+	// read BMP file
+	readbmp();//伪函数
+}
+
+void Screen::display_bmp(int bmp) {
+	displaybmp();//伪函数
+}
+
+PostureMonitor::PostureMonitor(Screen& screen) : screen(screen) {}
+
+void PostureMonitor::handlePostureChange(Buzzer& buzzer) {
+	bool postureChanged = true;
+	if (postureChanged) {
+		screen.displayText("Posture change");
+		buzzer.alarm();
+	}
+}
+
+int main(void)
+{
+	class Screen led();
+	class Buzzer Buz();
+	class PostureMonitor Pos_change_sender();
+	class PostureChange Pos_change_detector();
+}
