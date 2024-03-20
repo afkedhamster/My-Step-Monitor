bb70d92657d95c6a937ba310b48af8c4aa24463f
afkedhamster <58937489+afkedhamster@users.noreply.github.com>
2024-03-20 16:36:45 +0000

    Update Buzzer_wiringPi.cpp on chrome

--- Buzzer_wiringPi.cpp
+++ Buzzer_wiringPi.cpp
@@ -3,20 +3,11 @@
 #include <thread>
 #include <wiringPi.h> 
 
-// Òı½Å
-#define BUZZER_PIN 0 // °å×Ó
+// å¼•è„š
+#define BUZZER_PIN 0 // æ¿å­
 
-// ³õÊ¼»¯
+// åˆå§‹åŒ–
 void wiringPi_st()
-{
-    if (wiringPiSetup() == -1) {
-        std::cerr << "Failed to initialize WiringPi library" << std::endl;
-        exit(1);
-    }
-}
-
-// ³õÊ¼»¯
-void testwiringPi_st()
 {
     if (wiringPiSetup() == -1) {
         std::cerr << "Failed to initialize WiringPi library" << std::endl;
@@ -30,17 +21,17 @@ void testFunctionNewBrunch() {
 
 void setup()
 {
-    pinMode(BUZZER_PIN, OUTPUT); // Êä³ö
+    pinMode(BUZZER_PIN, OUTPUT); // è¾“å‡º
 }
 
-// ·¢Éù
+// å‘å£°
 void loop(int frequency, int duration)
 {
     for (int i = 0; i < duration / 2; i++) {
-        digitalWrite(BUZZER_PIN, LOW); // ·¢ÉùÒô
-        delay(1000 / frequency); // ÑÓÊ±1ms 
-        digitalWrite(BUZZER_PIN, HIGH); // ²»·¢ÉùÒô
-        delay(1000 / frequency); // ÑÓÊ±1ms 
+        digitalWrite(BUZZER_PIN, LOW); // å‘å£°éŸ³
+        delay(1000 / frequency); // å»¶æ—¶1ms 
+        digitalWrite(BUZZER_PIN, HIGH); // ä¸å‘å£°éŸ³
+        delay(1000 / frequency); // å»¶æ—¶1ms 
     }
 }
 
@@ -48,6 +39,6 @@ int main()
 {
     wiringPi_st();
     setup();
-    loop(1000, 3000); // 1000Hz£¬1Ãë
+    loop(1000, 3000); // 1000Hzï¼Œ1ç§’
     return 0;
 }
