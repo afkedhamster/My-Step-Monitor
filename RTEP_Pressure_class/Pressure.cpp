c72d1a1a3d952983e07edc852605c4169f5c1db2
Lzy <zhiyuelu23@163.com>
2024-03-20 20:38:04 +0000

    changes: pressure sensor can read digital input

--- RTEP_Pressure_class/Pressure.cpp
+++ RTEP_Pressure_class/Pressure.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <wiringPi.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
 
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#define pin 7
+//int vaule;
+int main(){
+    wiringPiSetup();
+    pinMode(pin,INPUT);
+    while(1)
+    {
+        if(digitalRead(pin)==0)
+        {
+        //有压力低电平
+             puts("有压力...");
+             delay(1000);
+             //vaule = analogread(pin)
+             //puts(vaule);
+        }
+	}
+    return 0;
+}
