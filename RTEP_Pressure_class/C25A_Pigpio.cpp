297972cf0476504dea5c78619a5532cb6cc50b6a
ShuaifeiWang5173 <2936142W@student.gla.ac.uk>
2024-04-09 22:45:40 +0800

    Pigpio

--- RTEP_Pressure_class/C25A_Pigpio.cpp
+++ RTEP_Pressure_class/C25A_Pigpio.cpp
@@ -7,22 +7,25 @@
 #define ADS_ADDR 0x48 
 #define ADS_REG_POINTER_CONVERT 0x00
 #define ADS_REG_POINTER_CONFIG  0x01
-// Parameters
-#define ADS_REG_CONFIG_DEFAULT \
-    (ADS_REG_CONFIG_CQUE_NONE | \
-     ADS_REG_CONFIG_CLAT_NONLAT | \
-     ADS_REG_CONFIG_CPOL_ACTVLOW | \
-     ADS_REG_CONFIG_CMODE_TRAD | \
-     ADS_REG_CONFIG_DR_128SPS | \
-     ADS_REG_CONFIG_MODE_SINGLE)
-#define ADS_REG_CONFIG_PGA_RANGE ADS1015_REG_CONFIG_PGA_4_096V
+// Book
+#define ADS1015_REG_CONFIG_CQUE_NONE    (0x0003)
+#define ADS1015_REG_CONFIG_CLAT_NONLAT  (0x0000)
+#define ADS1015_REG_CONFIG_CPOL_ACTVLOW (0x0000)
+#define ADS1015_REG_CONFIG_CMODE_TRAD   (0x0000)
+#define ADS1015_REG_CONFIG_DR_860SPS    (0x0080)
+#define ADS1015_REG_CONFIG_MODE_SINGLE  (0x0100)
+#define ADS1015_REG_CONFIG_PGA_4_096V   (0x0200)
+#define ADS1015_REG_CONFIG_MUX_SINGLE_0 (0x4000)
+#define ADS1015_REG_CONFIG_MUX_SINGLE_1 (0x5000)
+#define ADS1015_REG_CONFIG_MUX_SINGLE_2 (0x6000)
+#define ADS1015_REG_CONFIG_MUX_SINGLE_3 (0x7000)
+#define ADS1015_REG_CONFIG_OS_SINGLE    (0x8000)
 
-
-// Variables 
-    int i2c_handle;
+// Variable
+int i2c_handle;
 // Pins
-    const int SDA_PIN = 2; 
-    const int SCL_PIN = 3;
+const int SDA_PIN = 2; 
+const int SCL_PIN = 3;
 
 // ADS On/Off
 int ADS_init() 
@@ -33,7 +36,7 @@ int ADS_init()
         std::cerr << "Failed to initialize pigpio." << std::endl;
         return 1;
     }
-    // Input
+    // Prepare
     gpioSetMode(SDA_PIN, PI_INPUT);
     gpioSetMode(SCL_PIN, PI_INPUT);
     // Initialize I2C
@@ -76,36 +79,69 @@ int ADS_writeReg(unsigned char reg, unsigned int value)
 }
 
 // Parameters
-int ADS_measure() 
+int ADS_measure(int chan) 
 {
-    // Set
-    uint16_t mux = 0b0101000000000000; // "1+GND":5
-    uint16_t pga = 0b0000100000000000; // "8":0.25mV
-    uint16_t dr = 0b0000000010000000; // "128SPS"
-
-    uint16_t config = (mux | pga | dr);
-    // Write
-    if (ADS_writeReg(0x01, config) == -1) 
+    // Default
+    int config = ADS1015_REG_CONFIG_CQUE_NONE    |
+                 ADS1015_REG_CONFIG_CLAT_NONLAT  |
+                 ADS1015_REG_CONFIG_CPOL_ACTVLOW |
+                 ADS1015_REG_CONFIG_CMODE_TRAD   |
+                 ADS1015_REG_CONFIG_DR_860SPS   |
+                 ADS1015_REG_CONFIG_MODE_SINGLE |
+                 ADS1015_REG_CONFIG_PGA_4_096V;
+    // Input Channel
+    switch (chan) 
+    {
+        case (0):
+            config |= ADS1015_REG_CONFIG_MUX_SINGLE_0;
+            break;
+        case (1):
+            config |= ADS1015_REG_CONFIG_MUX_SINGLE_1;
+            break;
+        case (2):
+            config |= ADS1015_REG_CONFIG_MUX_SINGLE_2;
+            break;
+        case (3):
+            config |= ADS1015_REG_CONFIG_MUX_SINGLE_3;
+            break;
+    }
+    // Bit
+    config |= ADS1015_REG_CONFIG_OS_SINGLE;
+    // Send Config
+    config = ((config >> 8) & 0x00FF) | ((config << 8) & 0xFF00);
+    ADS_writeReg(ADS1015_REG_POINTER_CONFIG, config);
+    // Wait
+    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+    // Read
+    ADS_writeReg(ADS_REG_POINTER_CONVERT, 0); // Start conversion
+    char data[2];
+    if (i2cReadDevice(i2c_handle, data, 2) != 2) 
     {
         return -1; // Error
     }
-    // Return
-    return ADS_readReg(0x00);
+    int value = ((data[0] << 8) & 0xFF00) | data[1];
+    // Check
+    if (value > 0x7FFF) 
+    {
+        return (value - 0xFFFF);
+    } else {
+        return value;
+    }
 }
 
 int main() 
 {
     if (ADS_init() != 0) 
     {
-       std::cerr << "Initialization failed" << std::endl;
+        std::cerr << "Initialization failed" << std::endl;
         return -1;
     }
 
     while (true) 
     {
         // Read
-        int ret0 = ADS_measure();
-        int ret1 = ADS_measure();
+        int ret0 = ADS_measure(0);
+        int ret1 = ADS_measure(1);
         ret0 = ret0 > 32768 ? 0 : ret0;
         double ret_v0 = ret0 / 32768.0 * 6.144;
         ret1 = ret1 > 32768 ? 0 : ret1;
