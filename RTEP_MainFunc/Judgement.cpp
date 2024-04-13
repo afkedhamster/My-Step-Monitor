f662a5a61877eb894bba3fb6d21786a87b831c05
tangjiahui47 <tangjiahui02@126.com>
2024-04-13 20:27:53 +0100

    modify the judgment function

--- RTEP_MainFunc/Judgement.cpp
+++ RTEP_MainFunc/Judgement.cpp
@@ -75,7 +75,6 @@ void Judgment::Read()
     }
 }
 
-
 void Judgment::start_posEstimation() 
 {
     thread = std::thread(&Judgment::posEstimation, this);
@@ -92,115 +91,82 @@ void Judgment::restart_posEstimation()
     start_posEstimation();
 }
 
-void updatePose(std::string new_pose) 
-{
-    previous_pose = new_pose;
-}
-
 void Judgment::posEstimation()
 {   
         // the loop that will run forever
     while(1)
-    {
-        // the conditions that will trigger the pos
-        // if the acceleration is greater than the threshold and the pressure is greater than the threshold
-        if (pressure1 > pressure_threshold)
         {
+            // Save the current pose
+             previous_pose = POS;
+            
+            // the conditions that will trigger the pos
+            // if the acceleration is greater than the threshold and the pressure is greater than the threshold
+            if (pressure1 > pressure_threshold)
+            {
+                if((accelX_g > acc_threshold && accelY_g > acc_threshold) || (accelY_g > acc_threshold && accelZ_g > acc_threshold) || (accelX_g > acc_threshold && accelZ_g > acc_threshold))
+                {
+                    POS = FALL;  // if the acceleration is greater than the threshold and the pressure is greater than the threshold, then the pos is FALL
+                }
+            }
+            if (pressure1 > pressure_threshold)
+            {
+                if((gyroX_degPerSec > 0.8 * gyro_threshold && gyroY_degPerSec > 0.8 * gyro_threshold) || (gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold) || (gyroX_degPerSec> 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold))
+                {
+                    POS = FALL;
+                }
+            }
             if((accelX_g > acc_threshold && accelY_g > acc_threshold) || (accelY_g > acc_threshold && accelZ_g > acc_threshold) || (accelX_g > acc_threshold && accelZ_g > acc_threshold))
             {
-                POS = FALL;  // if the acceleration is greater than the threshold and the pressure is greater than the threshold, then the pos is FALL
+                if((gyroX_degPerSec > gyro_threshold && gyroY_degPerSec > gyro_threshold) || (gyroY_degPerSec > gyro_threshold && gyroZ_degPerSec > gyro_threshold) || (gyroX_degPerSec > gyro_threshold && gyroZ_degPerSec > gyro_threshold))
+                {
+                    POS = FALL;
+                }
             }
-        }
-        if (pressure1 > pressure_threshold)
-        {
-            if ((gyroX_degPerSec > 0.8 * gyro_threshold && gyroY_degPerSec > 0.8 * gyro_threshold) || (gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold) || (gyroX_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold))
+            
+            // if the acceleration is less than the threshold and the pressure is greater than the threshold
+            if(accelX_g < 0.01 && accelY_g < 0.01 && accelZ_g < 0.01 && pressure1 > pressure_threshold)
             {
-                POS = FALL;
+                POS = STAND; // if the acceleration is less than the threshold and the pressure is greater than the threshold, then the pos is STAND
             }
-        }
-        if ((accelX_g > acc_threshold && accelY_g > acc_threshold) || (accelY_g > acc_threshold && accelZ_g > acc_threshold) || (accelX_g > acc_threshold && accelZ_g > acc_threshold))
-        {
-            if ((gyroX_degPerSec > gyro_threshold && gyroY_degPerSec > gyro_threshold) || (gyroY_degPerSec > gyro_threshold && gyroZ_degPerSec > gyro_threshold) || (gyroX_degPerSec > gyro_threshold && gyroZ_degPerSec > gyro_threshold))
+
+            // if the acceleration is less than the threshold and the pressure is less than the threshold
+            if(accelX_g < 0.01 && accelY_g < 0.01 && accelZ_g < 0.01 && pressure1 < 0.5 * pressure_threshold && pressure2 < 0.5 * pressure_threshold)
             {
-                POS = FALL;
+                POS = SITDOWN; // if the acceleration is less than the threshold and the pressure is less than the threshold, then the pos is SITDOWN
             }
-        }
 
-        // if the acceleration is less than the threshold and the pressure is greater than the threshold
-        if (accelX_g < 0.01 && accelY_g < 0.01 && accelZ_g < 0.01 && pressure1 > pressure_threshold)
-        {
-            POS = STAND; // if the acceleration is less than the threshold and the pressure is greater than the threshold, then the pos is STAND
-        }
-
-        // if the acceleration is greater than the threshold and the pressure is greater than the threshold
-        if ((accelX_g > acc_threshold || accelY_g > acc_threshold || accelZ_g > acc_threshold) && (pressure1 > 0.4 * pressure_threshold))
-        {
-            usleep(1000000); // sleep for 1s
-            if ((pressure1 > 0.4 * pressure_threshold))
+            // if the acceleration is less than the threshold and the pressure is less than the threshold
+            if((pressure1 < 0.2 * pressure_threshold) && ((gyroX_degPerSec > 0.8 * gyro_threshold && gyroY_degPerSec > 0.8 * gyro_threshold)||(gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold)||(gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold)) && (accelX_g < acc_threshold && accelY_g < acc_threshold && accelZ_g < acc_threshold))
             {
-                POS = WALKING; // if the acceleration is greater than the threshold and the pressure is greater than the threshold, then the pos is WALKING
+                POS = LAYDOWN; // if the acceleration is less than the threshold and the pressure is less than the threshold, then the pos is LAYDOWN
             }
-        }
 
-        // if the acceleration is less than the threshold and the pressure is less than the threshold
-        if (accelX_g < 0.01 && accelY_g < 0.01 && accelZ_g < 0.01 && pressure1 < 0.5 * pressure_threshold && pressure2 < 0.5 * pressure_threshold)
-        {
-            POS = SITDOWN; // if the acceleration is less than the threshold and the pressure is less than the threshold, then the pos is SITDOWN
-        }
+            std::string poseChange = "NO_CHANGE";
+            if (previous_pose != POS) {
+            if (previous_pose == "SITDOWN" && POS == "STAND") {
+                posChange = "SIT2STAND";
+            }
+            if (previous_pose == "STAND" && POS == "SITDOWN") {
+                posChange = "STAND2SIT";
+            }
+            if (previous_pose == "SITDOWN" && POS == "LAYDOWN") {
+                posChange = "SIT2LAY";
+            }
+            if (previous_pose == "STAND" && POS == "LAYDOWN") {
+                posChange = "STAND2LAY";
+            }
+            if (previous_pose == "FALL" && POS == "STAND") {
+                posChange = "RISE";
+            }
+            if (previous_pose == "FALL" && POS == "SITDOWN") {
+                posChange = "FALL";
+            }
+            if (previous_pose == "FALL" && POS == "LAYDOWN") {
+                posChange = "FALL";
+            }
+            }
 
-        // if the acceleration is less than the threshold and the pressure is less than the threshold
-        if ((pressure1 < 0.2 * pressure_threshold) && ((gyroX_degPerSec > 0.8 * gyro_threshold && gyroY_degPerSec > 0.8 * gyro_threshold) || (gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold) || (gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold)) && (accelX_g < acc_threshold && accelY_g < acc_threshold && accelZ_g < acc_threshold))
-        {
-            POS = LAYDOWN; // if the acceleration is less than the threshold and the pressure is less than the threshold, then the pos is LAYDOWN
+            // sleep for 100ms
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-
-        // Check for pose change
-        std::string current_pose = getCurrentPose(); // You need to implement this method
-        if (previous_pose == "SITDOWN" && current_pose == "STAND")
-        {
-            posChange = "SIT2STAND";
-        }
-        if (previous_pose == "STAND" && current_pose == "SITDOWN")
-        {
-            posChange = "STAND2SIT";
-        }
-        if (previous_pose == "SITDOWN" && current_pose == "LAYDOWN")
-        {
-            posChange = "SIT2LAY";
-        }
-        if (previous_pose == "LAYDOWN" && current_pose == "SITDOWN")
-        {
-            posChange = "LAY2SIT";
-        }
-        if (previous_pose == "LAYDOWN" && current_pose == "STAND")
-        {
-            posChange = "LAY2STAND";
-        }
-        if (previous_pose == "STAND" && current_pose == "LAYDOWN")
-        {
-            posChange = "STAND2LAY";
-        }
-        if (previous_pose == "FALL" && current_pose == "STAND")
-        {
-            posChange = "RISE";
-        }
-        if (previous_pose == "FALL" && current_pose == "SITDOWN")
-        {
-            posChange = "FALL";
-        }
-        if (previous_pose == "FALL" && current_pose == "LAYDOWN")
-        {
-            posChange = "FALL";
-        }
-        // Update the previous pose at the end of the method
-        updatePose(current_pose);
-
-        // sleep for 100ms
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
 };
-
-void Judgment::updatePose(std::string new_pose) 
-{
-    previous_pose = new_pose;
-}
