2922a6c1a63897a48e2c689f31512f21f4baf00a
ShuaifeiWang5173 <2936142W@student.gla.ac.uk>
2024-04-13 22:55:46 +0800

    C25A Commu

--- RTEP_Commu/C25A_Commu.cpp
+++ RTEP_Commu/C25A_Commu.cpp
@@ -2,72 +2,58 @@
 #include <chrono>
 #include <thread>
 #include <cstring> 
-#include "ADS1115_Wrapper.h"
+#include "ADS1115.h"
 #include "IPC.h"
-
-// Define Message
-struct Message 
-{
-    int ret0;
-    double ret_v0;
-    int ret1;
-    double ret_v1;
-};
-
 
 int main() 
 {
-    // Wrapper
-    ADS1115_Wrapper ads;
+    ADS1115 ADS;
+    ADS.ADS_init();
 
-    // Initialization ADS
-    if (!ads.init(1)) 
+    // Mark B
+    IPC ipc("/tmp", 'B'); 
+    if (!ipc.MsgID("/tmp", 'B')) 
     {
-        std::cerr << "Initialization failed" << std::endl;
-        return -1;
-    }
-
-    // Access Queue
-    int msgid = getOrCreateMessageQueue("/tmp", 'B'); // Mark B
-    if (msgid == -1) 
-    {
-        std::cerr << "Failed to create or access message queue." << std::endl;
+        std::cerr << "Failed to set message queue identifier." << std::endl;
         return -1;
     }
 
     while (true) 
     {
-        // Process
-        ads.Process(ret0, ret_v0, ret1, ret_v1);
-        
+        // Set (Different Channels)
+        float ret0 = ADS.ADS_measure(ADS1115_REG_CONFIG_MUX_SINGLE_0, 
+                                ADS1115_REG_CONFIG_PGA_2_048V, 
+                                ADS1115_REG_CONFIG_DR_860SPS);
+        /* float ret1 = ADS_measure(ADS1115_REG_CONFIG_MUX_SINGLE_1, 
+                                ADS1115_REG_CONFIG_PGA_2_048V, 
+                                ADS1115_REG_CONFIG_DR_860SPS); */
+        ret0 = ret0 > 32768 ? 0 : ret0;
+        float ret_v0 = (ret0*2.048)/32768;
+        /* ret1 = ret1 > 32768 ? 0 : ret1;
+        float ret_v1 = (ret1*2.048)/32768; */
+
         // Perpare Message
-        Message message;
-        message.accelX_g = accelX_g;
-        message.accelY_g = accelY_g;
-        message.accelZ_g = accelZ_g;
-        message.gyroX_degPerSec = gyroX_degPerSec;
-        message.gyroY_degPerSec = gyroY_degPerSec;
-        message.gyroZ_degPerSec = gyroZ_degPerSec;
+        std::vector<float> data = {ret0, ret_v0};
+        Message message = createMessage(data);
 
         // Send
-        if (msgsnd(msgid, &message, sizeof(message) - sizeof(long), 0) == -1) 
+        if (!ipc.send(message)) 
         {
             std::cerr << "Failed to send message to message queue." << std::endl;
             return -1;
         }
 
         // Display
-        std::cout << "Message sent to message queue." << std::endl;
         std::cout << "ret0 = " << ret0 << std::endl;
         std::cout << "ret_v0 = " << ret_v0 << std::endl;
-        std::cout << "ret1 = " << ret1 << std::endl;
-        std::cout << "ret_v1 = " << ret_v1 << std::endl;
+        /* std::cout << "ret1 = " << ret1 << std::endl;
+        std::cout << "ret_v1 = " << ret_v1 << std::endl; */
         
         // Delay
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
-    ads.stop();
+    ADS.ADS_stop();
 
     return 0;
 }
