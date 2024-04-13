9fd5e786624005bd12e03f06152e474624f75c40
ShuaifeiWang5173 <2936142W@student.gla.ac.uk>
2024-04-14 02:02:17 +0800

    Merge classes in "Threads_read_detect.cpp"

---
+++ RTEP_MainFunc/Judgment.h
@@ -0,0 +1,42 @@
+#ifndef JUDGMENT_H
+#define JUDGMENT_H
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include "IPC.h"
+
+class Judgment {
+public:
+    enum POSTURE 
+    {
+        FALL,
+        STAND,
+        WALKING,
+        SITDOWN,
+        LAYDOWN
+    };
+
+    void start_Read();
+    void stop_Read();
+    void restart_Read();
+    void start_posEstimation();
+    void stop_posEstimation();
+    void restart_posEstimation();
+    void Read();
+    void posEstimation();
+    std::thread thread;
+    std::string previous_pose;
+
+    POSTURE POS;
+    float accelX_g = 0, accelY_g = 0, accelZ_g = 0;
+    float gyroX_degPerSec = 0, gyroY_degPerSec = 0, gyroZ_degPerSec = 0;
+    float pressure1 = 0;
+
+    float pressure_threshold = 0.5;
+    float acc_threshold = 0.1;
+    float gyro_threshold = 0.1;
+};
+
+#endif
