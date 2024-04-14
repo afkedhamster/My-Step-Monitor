5fbc6f57d16adfca74383b11811d603dc19e4a09
tangjiahui47 <156908629+tangjiahui47@users.noreply.github.com>
2024-04-14 04:39:12 +0100

    Change staging struction of posture change

--- RTEP_MainFunc/Threads_read_detect.cpp
+++ RTEP_MainFunc/Threads_read_detect.cpp
@@ -5,11 +5,17 @@
 #include <thread>
 #include <chrono>
 
-enum POSTURE{
+enum POS_CHANGE{
     FALL,
     STAND,
-    SITDOWN,
-    LAYDOWN
+    // WALKING,
+    SIT,
+    LAY,
+
+    RISE,
+    SIT2LAY,
+    STAND2SIT,
+    STAND2LAY,
 };
 
 class Threads_Read{
@@ -72,82 +78,98 @@ public:
     };
     
     // Add a string to store the previous pose
-   std::string previous_pose = "UNKNOWN";
+    std::string previous_pose;
+
+    // Add a method to update the previous pose
+    void updatePose(std::string new_pose) {
+        previous_pose = new_pose;
+    }
 
     // the function that the thread will run
     void doSomething()
     {   
+         enum POS_CHANGE previous_pos = STAND; // the previous pos
+        
         // the loop that will run forever
         while(1)
         {
-            // Save the current pose
-             previous_pose = POS;
-            
-            // the conditions that will trigger the pos
-            // if the acceleration is greater than the threshold and the pressure is greater than the threshold
+            // FALL
             if (pressure1 > pressure_threshold)
             {
                 if((accelX_g > acc_threshold && accelY_g > acc_threshold) || (accelY_g > acc_threshold && accelZ_g > acc_threshold) || (accelX_g > acc_threshold && accelZ_g > acc_threshold))
                 {
-                    POS = FALL;  // if the acceleration is greater than the threshold and the pressure is greater than the threshold, then the pos is FALL
+                    if(previous_pos == STAND)
+                    {
+                        posChange = FALL;
+                        previous_pos = FALL;
+                    }
                 }
             }
             if (pressure1 > pressure_threshold)
             {
                 if((gyroX_degPerSec > 0.8 * gyro_threshold && gyroY_degPerSec > 0.8 * gyro_threshold) || (gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold) || (gyroX_degPerSec> 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold))
                 {
-                    POS = FALL;
+                    if(previous_pos == STAND)
+                    {
+                        posChange = FALL;
+                        previous_pos = FALL;
+                    }
                 }
             }
             if((accelX_g > acc_threshold && accelY_g > acc_threshold) || (accelY_g > acc_threshold && accelZ_g > acc_threshold) || (accelX_g > acc_threshold && accelZ_g > acc_threshold))
             {
                 if((gyroX_degPerSec > gyro_threshold && gyroY_degPerSec > gyro_threshold) || (gyroY_degPerSec > gyro_threshold && gyroZ_degPerSec > gyro_threshold) || (gyroX_degPerSec > gyro_threshold && gyroZ_degPerSec > gyro_threshold))
                 {
-                    POS = FALL;
+                    if(previous_pos == STAND)
+                    {
+                        posChange = FALL;
+                        previous_pos = FALL;
+                    }
                 }
             }
             
-            // if the acceleration is less than the threshold and the pressure is greater than the threshold
+            // STAND
             if(accelX_g < 0.01 && accelY_g < 0.01 && accelZ_g < 0.01 && pressure1 > pressure_threshold)
             {
-                POS = STAND; // if the acceleration is less than the threshold and the pressure is greater than the threshold, then the pos is STAND
+                if(previous_pos == FALL)
+                {
+                    posChange = RISE;
+                    previous_pos = STAND;
+                }
             }
 
-            // if the acceleration is less than the threshold and the pressure is less than the threshold
+            // WALKING
+            // if((accelX_g > acc_threshold || accelY_g > acc_threshold || accelZ_g > acc_threshold) && (pressure1 > 0.4 * pressure_threshold))
+            // {
+            //     usleep(1000000); // sleep for 1s
+            //     if((pressure1 > 0.4 * pressure_threshold))
+            //     {
+            //         POS = WALKING;  
+            //     }
+            // }
+
+            // SIT
             if(accelX_g < 0.01 && accelY_g < 0.01 && accelZ_g < 0.01 && pressure1 < 0.5 * pressure_threshold && pressure2 < 0.5 * pressure_threshold)
             {
-                POS = SITDOWN; // if the acceleration is less than the threshold and the pressure is less than the threshold, then the pos is SITDOWN
+                if(previous_pos == STAND)
+                {
+                    posChange = STAND2SIT;
+                    previous_pos = SIT;
+                }
             }
 
-            // if the acceleration is less than the threshold and the pressure is less than the threshold
+            // LAY
             if((pressure1 < 0.2 * pressure_threshold) && ((gyroX_degPerSec > 0.8 * gyro_threshold && gyroY_degPerSec > 0.8 * gyro_threshold)||(gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold)||(gyroY_degPerSec > 0.8 * gyro_threshold && gyroZ_degPerSec > 0.8 * gyro_threshold)) && (accelX_g < acc_threshold && accelY_g < acc_threshold && accelZ_g < acc_threshold))
             {
-                POS = LAYDOWN; // if the acceleration is less than the threshold and the pressure is less than the threshold, then the pos is LAYDOWN
-            }
-
-            // Check for pose change
-            std::string poseChange = "NO_CHANGE";
-            if (previous_pose != POS) {
-            if (previous_pose == "SITDOWN" && POS == "STAND") {
-                posChange = "SIT2STAND";
-            }
-            if (previous_pose == "STAND" && POS == "SITDOWN") {
-                posChange = "STAND2SIT";
-            }
-            if (previous_pose == "SITDOWN" && POS == "LAYDOWN") {
-                posChange = "SIT2LAY";
-            }
-            if (previous_pose == "STAND" && POS == "LAYDOWN") {
-                posChange = "STAND2LAY";
-            }
-            if (previous_pose == "FALL" && POS == "STAND") {
-                posChange = "RISE";
-            }
-            if (previous_pose == "FALL" && POS == "SITDOWN") {
-                posChange = "FALL";
-            }
-            if (previous_pose == "FALL" && POS == "LAYDOWN") {
-                posChange = "FALL";
+                if(previous_pos == STAND)  
+                {
+                    posChange = STAND2LAY;  
+                    previous_pos = LAY;
+                }
+                else if(previous_pos == SIT)
+                {
+                    posChange = SIT2LAY;
+                    previous_pos = LAY;
             }
             }
 
@@ -157,7 +179,7 @@ public:
     };
     std::thread thread;  // the thread
 public:
-    enum POSTURE POS;                     // the pos
+    enum POS_CHANGE posChange; 
     float accelX_g = 0, accelY_g = 0, accelZ_g = 0;         // acceleration
     float gyroX_degPerSec = 0, gyroY_degPerSec = 0, gyroZ_degPerSec = 0;      // angular velocity
     float pressure1 = 0;            // pressure
